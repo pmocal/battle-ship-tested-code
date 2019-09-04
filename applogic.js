225925ade3b7737cbfe4b236d0ec81cc88607545
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-09-04 16:04:29 -0400

    first commit

---
+++ applogic.js
@@ -0,0 +1,40 @@
+const Ship = function(length) {
+	//length, where theyve been hit, whether its sunk
+	var positions = new Array(length);
+	const hit = function(number){
+		//marks the ship as hit at that index
+		positions[number] = 'X';
+	}
+	const isSunk = function() {
+		//all positions hit
+		var flag = true;
+		for (let position in positions) {
+			if (position != 'X') {
+				flag = false;
+			}
+		}
+		if (flag) {
+			return true;
+		}
+	}
+}
+
+const Gambeboard = function(dimensions) {
+	var rows = new Array(dimensions[1]);
+	for (let row in rows) {
+		row = new Array(dimensions[2]);
+	}
+	const placeShip = function(ship, x, y) {
+		//rows[y][x] is location that ship is placed at
+		//walk through nearby spaces and place ship
+		//or figure out that ship can't be placed?
+
+	}
+	const receiveAttack = function() {
+		//either hit ship or record coordinates of the miss
+	}
+	//missed attacks tracked so later they can be
+	//displayed on board
+
+	//board can report if all ships sunk
+}
