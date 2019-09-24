c67f43e8bb6c7b641782796441aa72048a96a22a
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-09-24 16:35:03 -0400

    https://github.com/pmocal/battle-ship-tested-code/issues/11

--- test/gameboard.js
+++ test/gameboard.js
@@ -1,28 +1,56 @@
 const t = require('tap');
 const battleshiptestedcode = require('../src/applogic.js');
-var shipLength = 5;
-var testShip = battleshiptestedcode['Ship'](shipLength);
-var testBoard = battleshiptestedcode['Gameboard']([testShip], [10, 10]);
+
+function setup() {
+	var shipLength = 5;
+	var testShip = battleshiptestedcode['Ship'](shipLength);
+	var testBoard = battleshiptestedcode['Gameboard']([testShip], [10, 10]);
+	return [ testBoard, testShip, shipLength ];
+}
 
 t.test("receiveAttack method with a missed shot", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
 	t.same(testBoard.receiveAttack(0, 5), [0, 5]);
 	t.end();
 });
 
 t.test("receiveAttack method which sends hit(i) function to ship", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
 	t.same(testBoard.receiveAttack(0, 0).positions, ['X',null,null,null,null]);
 	t.end();
 });
 
 t.test("receiveAttack method sends a second hit(i) function to ship", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
+	t.same(testBoard.receiveAttack(0, 0).positions, ['X',null,null,null,null]);
 	t.same(testBoard.receiveAttack(0, 1).positions, ['X','X',null,null,null]);
 	t.end();
 });
 
 t.test("after a final hit, ship's isSunk() evaluates to true", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
 	t.equals(testShip.isSunk(), false);
+	testBoard.receiveAttack(0, 0);
+	testBoard.receiveAttack(0, 1);
 	testBoard.receiveAttack(0, 2);
 	testBoard.receiveAttack(0, 3);
 	t.same(testBoard.receiveAttack(0, 4).isSunk(), true);
+	t.end();
+});
+
+t.test("shipsAllSunk() evaluates to false", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
+	testBoard.receiveAttack(0, 0);
+	testBoard.receiveAttack(0, 1);
+	testBoard.receiveAttack(0, 2);
+	t.same(testBoard.shipsAllSunk(), false);
+	testBoard.receiveAttack(0, 3);
+	testBoard.receiveAttack(0, 4);
+	t.same(testBoard.shipsAllSunk(), true);
+	t.end();
+});
+
+t.test("renderBoard(boardId) adds ", t => {
+	var [ testBoard, testShip, shipLength ] = setup();
 	t.end();
 });
--- test/ship.js
+++ test/ship.js
@@ -1,25 +1,33 @@
 const t = require('tap');
 const battleshiptestedcode = require('../src/applogic.js');
-var shipLength = 5;
-var testShip = battleshiptestedcode['Ship'](shipLength);
+
+function setup(){
+	var shipLength = 5;
+	var testShip = battleshiptestedcode['Ship'](shipLength);
+	return [ testShip, shipLength ];
+}
 
 t.test("hit method", t => {
+	var [ testShip, shipLength ] = setup();
 	t.same(testShip.hit(4).positions, [,,,,'X']);
 	t.end();
 });
 
 t.test("hit method multiple times", t => {
+	var [ testShip, shipLength ] = setup();
 	t.same(testShip.hit(4).positions, [null,null,null,null,'X']);
 	t.same(testShip.hit(0).positions, ['X',null,null,null,'X']);
 	t.end();
 });
 
 t.test("isSunk method false", t => {
+	var [ testShip, shipLength ] = setup();
 	t.same(testShip.isSunk(), false);
 	t.end();
 });
 
 t.test("isSunk method true", t => {
+	var [ testShip, shipLength ] = setup();
 	for (let i = 0; i < shipLength; i++) {
 		testShip.hit(i);
 	}
